27292bc5fdf57cad4e9f874e6304b9e8ddf4dcd3
donnysppl <donny@sppl.ind.in>
2023-08-22 12:49:12 +0530

    adding Access-Control-Allow-Origin in app.js

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ const bodyParser = require('body-parser');
 var cors = require('cors');
 app.use(cors());
 
-app.use(express.json({limit:"50mb"}));
-app.use(express.urlencoded({limit:"50mb", extended : true}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(bodyParser.json({limit:"50mb"}));
-app.use(bodyParser.urlencoded({limit:"50mb", extended : true}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // use passport initiallization here
 const passport = require('passport');
 app.use(passport.initialize());
 
 const path = require('path');
-require('dotenv').config({  
-    path: path.join(__dirname,'.env')
+require('dotenv').config({
+    path: path.join(__dirname, '.env')
 })
 
 const userRoute = require('./routes/users');
@@ -27,12 +27,17 @@ const adminRoute = require('./routes/admins');
 
 const productRoute = require('./routes/products');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://services.shopsppl.org');
+    next();
+});
 
-app.use('/users',userRoute)
 
-app.use('/admins',adminRoute)
+app.use('/users', userRoute)
 
-app.use('/products',productRoute)
+app.use('/admins', adminRoute)
+
+app.use('/products', productRoute)
 
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
